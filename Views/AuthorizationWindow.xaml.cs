cb5978eea830f295b9b8f628dca97d39261ef28d
RedMarshal <redmarshal37@gmail.com>
2022-01-20 21:34:00 +0300

    add authorisation screen

---
+++ Views/AuthorizationWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Equality.Views
+{
+    public partial class AuthorizationWindow
+    {
+        public AuthorizationWindow() : base (Catel.Windows.DataWindowMode.Custom)
+        {
+            InitializeComponent();
+        }
+    }
+}
