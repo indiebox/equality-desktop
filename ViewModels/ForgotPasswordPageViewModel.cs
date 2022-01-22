f4fda6cce88c3919cfc8c7e3ffa19608fd08592d
RedMarshal <redmarshal37@gmail.com>
2022-01-22 16:28:01 +0300

    Create forgot password page

---
+++ ViewModels/ForgotPasswordPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+using Catel.MVVM;
+
+namespace Equality.ViewModels
+{
+    public class ForgotPasswordPageViewModel : ViewModelBase
+    {
+        public ForgotPasswordPageViewModel(/* dependency injection here */)
+        {
+        }
+
+        public override string Title { get { return "View model title"; } }
+
+        // TODO: Register models with the vmpropmodel codesnippet
+        // TODO: Register view model properties with the vmprop or vmpropviewmodeltomodel codesnippets
+        // TODO: Register commands with the vmcommand or vmcommandwithcanexecute codesnippets
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            // TODO: subscribe to events here
+        }
+
+        protected override async Task CloseAsync()
+        {
+            // TODO: unsubscribe from events here
+
+            await base.CloseAsync();
+        }
+    }
+}
