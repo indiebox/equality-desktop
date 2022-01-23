4d60e7a56e205612ae610bdc62205ece4f316d57
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-01-23 13:58:30 +0300

    Add better design support, fix code style, use `INavigationService`

---
+++ ViewModels/Design/DesignLoginPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Equality.ViewModels.Design
+{
+    public class DesignLoginPageViewModel : LoginPageViewModel
+    {
+        public DesignLoginPageViewModel() : base()
+        {
+            Title = "Вход";
+        }
+    }
+}
