ac12f3687b66c2a31bf305763b96f24e927eb278
RedMarshal <redmarshal37@gmail.com>
2022-01-23 20:13:35 +0300

    Add restore password page

---
+++ ViewModels/ResetPasswordPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+using Catel.MVVM;
+
+namespace Equality.ViewModels.Design
+{
+    public class ResetPasswordPageViewModel : ViewModelBase
+    {
+        public ResetPasswordPageViewModel(/* dependency injection here */)
+        {
+        }
+
+        public override string Title => "Изменение пароля";
+
+        // TODO: Register models with the vmpropmodel codesnippet
+        // TODO: Register view model properties with the vmprop or vmpropviewmodeltomodel codesnippets
+        // TODO: Register commands with the vmcommand or vmcommandwithcanexecute codesnippets
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            // TODO: subscribe to events here
+        }
+
+        protected override async Task CloseAsync()
+        {
+            // TODO: unsubscribe from events here
+
+            await base.CloseAsync();
+        }
+    }
+}
---
+++ Views/ResetPasswordPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Equality.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ResetPasswordPage.xaml
+    /// </summary>
+    public partial class ResetPasswordPage
+    {
+        public ResetPasswordPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
