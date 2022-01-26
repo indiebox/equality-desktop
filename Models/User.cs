d29e158eaaa996ac58361df51c57bd937faffa9d
RedMarshal <redmarshal37@gmail.com>
2022-01-26 18:08:24 +0300

    RemoveUselessComments

--- Models/User.cs
+++ Models/User.cs
@@ -17,51 +17,20 @@ namespace Equality.Models
 {
     class User : ModelBase
     {
-
-        //private static readonly HttpClient client = new HttpClient();
-
-        //private static async Task Request()
-        //{
-        //    client.DefaultRequestHeaders.Accept.Clear();
-        //    client.DefaultRequestHeaders.Accept.Add(
-        //        new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-        //    client.DefaultRequestHeaders.Add("Accept", "application/json");
-        //    var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
-
-        //    var msg = await stringTask;
-        //    Console.Write(msg);
-        //}
-
         private static async Task<string> Request(string url, string method, Dictionary<string, string> Params, string accept = "application/json")
         {
             try {
-                //WebRequest request = WebRequest.Create(url);
-                //request.Method = method;
-                //request.ContentType = "application/json";
-                //byte[] byteArray = Encoding.UTF8.GetBytes(HttpUtility.UrlEncode("email=test@mail.ru password=123456 device_name=Desktop-XXX"));
-                //request.Headers.Add("Accept: application/json");
-                //request.ContentLength = byteArray.Length;
 
-                //using (Stream dataStream = request.GetRequestStream()) {
-                //    dataStream.Write(byteArray, 0, byteArray.Length);
-                //}
-                //string responseResult;
-                //WebResponse response = await request.GetResponseAsync();
-                //using (Stream stream = response.GetResponseStream()) {
-                //    using (StreamReader reader = new StreamReader(stream)) {
-                //        responseResult = reader.ReadToEnd();
-                //    }
-                //}
-                //response.Close();
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
                 request.Method = method;
                 request.Accept = accept;
                 NameValueCollection outgoingQueryString = HttpUtility.ParseQueryString(String.Empty);
-                byte[] byteArray = Encoding.UTF8.GetBytes(HttpUtility.UrlEncode("email=test@mail.ru password=123456 device_name=Desktop-XXX"));
-                using (Stream dataStream = request.GetRequestStream()) {
-                    dataStream.Write(byteArray, 0, byteArray.Length);
-                }
-                request.ContentLength = byteArray.Length;
+                //byte[] byteArray = Encoding.UTF8.GetBytes(HttpUtility.UrlEncode("email=test@mail.ru password=123456 device_name=Desktop-XXX"));
+                //using (Stream dataStream = request.GetRequestStream()) {
+                //    dataStream.Write(byteArray, 0, byteArray.Length);
+                //}
+                //request.ContentLength = byteArray.Length;
+
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
                 string responseText;
