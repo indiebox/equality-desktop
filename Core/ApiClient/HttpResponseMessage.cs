ca4487cdfaa6135e78be8b960c575b2508bff687
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-01-30 18:23:00 +0300

    Add docs in `HttpResponseMessage`

--- Core/ApiClient/HttpResponseMessage.cs
+++ Core/ApiClient/HttpResponseMessage.cs
@@ -6,8 +6,14 @@ namespace Equality.Core.ApiClient
 {
     public class ApiResponseMessage
     {
+        /// <summary>
+        /// Get the original response was returned after request.
+        /// </summary>
         public HttpResponseMessage Original { get; protected set; }
 
+        /// <summary>
+        /// Get the parsed content of response was returned after request.
+        /// </summary>
         public JObject Content { get; protected set; }
 
         public ApiResponseMessage(HttpResponseMessage original, JObject content)
