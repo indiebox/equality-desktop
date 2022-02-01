e18ec0739228addb1b9d02578b6147ce135de7ca
RedMarshal <redmarshal37@gmail.com>
2022-02-01 17:39:38 +0300

    Remove seless code

--- Models/User.cs
+++ Models/User.cs
@@ -24,7 +24,8 @@ namespace Equality.Models
             Errors = statusTextJson["errors"] as JObject;
         }
     }
-    class User : ModelBase
+
+    internal class User : ModelBase
     {
         public User(string name, string email, string password)
         {
@@ -36,35 +37,5 @@ namespace Equality.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
-        private static async Task<string> Request(string url, Dictionary<string, string> @params, string accept = "application/json")
-        {
-            HttpClient client = new();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(accept));
-            string result;
-            string strParams = JsonConvert.SerializeObject(@params);
-            Debug.Print(strParams);
-            HttpContent content = new StringContent(strParams, Encoding.UTF8, accept);
-
-            HttpResponseMessage response = await client.PostAsync(url, content);
-            result = await response.Content.ReadAsStringAsync();
-            if (response.IsSuccessStatusCode) {
-                return result;
-            } else if ((int)response.StatusCode == 422) {
-                throw new ValidationError((int)response.StatusCode, result);
-            } else {
-                throw new Exception(result);
-            }
-
-        }
-
-        public static async Task<string> Login(string email, string password, string deviceName)
-        {
-            Dictionary<string, string> Params = new Dictionary<string, string>();
-            Params.Add("email", email);
-            Params.Add("password", password);
-            Params.Add("device_name", deviceName);
-            return await Request("http://equality/api/v1/login", Params, "application/json");
-        }
     }
 }
