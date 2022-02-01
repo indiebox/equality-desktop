2e559569f52a0845a9f9b0cce80b9e2a002b342a
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-01 22:45:49 +0300

    Add register window

---
+++ Views/RegisterWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Equality.Core.CefSharp;
+
+namespace Equality.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterWindow
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+
+            // Disable context menu in browser by click RMB.
+            Browser.MenuHandler = new CustomMenuHandler();
+        }
+    }
+}
