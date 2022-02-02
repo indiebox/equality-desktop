ac68f91d54eea05860ae3a58b84da1e20ccb4ed4
RedMarshal <redmarshal37@gmail.com>
2022-02-02 18:50:27 +0300

    Add start page

---
+++ ViewModels/Design/DesignStartPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Equality.ViewModels.Design
+{
+    public class DesignStartPageViewModel : StartPageViewModel
+    {
+        public DesignStartPageViewModel() : base()
+        {
+            Name = "Hello, " +
+                "Peter";
+        }
+    }
+}
---
+++ ViewModels/StartPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Catel.MVVM;
+
+namespace Equality.ViewModels
+{
+    public class StartPageViewModel : ViewModelBase
+    {
+        public string Name { get; set; } = "Hello, Peter";
+
+        public StartPageViewModel()
+        {
+
+        }
+    }
+}
