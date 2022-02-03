36741ea8e3d99f9f0ad7ce278a2b5a526cbced1a
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-03 14:31:13 +0300

    Improve model `User`

--- Models/User.cs
+++ Models/User.cs
@@ -1,18 +1,27 @@
-﻿using Catel.Data;
+﻿using System;
+
+using Catel.Data;
 
 namespace Equality.Models
 {
     public class User : ModelBase
     {
-        public User(string name, string email, string password = "")
+        public User()
         {
-            Name = name;
-            Email = email;
-            Password = password;
         }
 
+        public ulong Id { get; set; }
+
         public string Name { get; set; }
+
         public string Email { get; set; }
-        public string Password { get; set; }
+
+        public DateTime? EmailVerifiedAt { get; set; }
+
+        public bool IsVerified => EmailVerifiedAt != null;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
     }
 }
