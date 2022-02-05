3c5b6b849ac82643b764d7e888af3829f872c1de
RedMarshal <redmarshal37@gmail.com>
2022-02-05 20:26:46 +0300

    Fix doc

--- Services/Interfaces/IUserService.cs
+++ Services/Interfaces/IUserService.cs
@@ -26,12 +26,12 @@ namespace Equality.Services
         /// <summary>
         /// Sends a request to the API to reset the password.
         /// </summary>
-        /// <param name="email">The user Email.</param>
+        /// <param name="email">The user email.</param>
         /// <param name="password">The user password.</param>
-        /// <param name="password_confirmation">The user re-entered password.</param>
+        /// <param name="passwordConfirmation">The user re-entered password.</param>
         /// <param name="token">A token from a letter sent to an email.</param>
         /// <returns>Returns the server response as a <see cref="JObject"/>.</returns>
-        public Task<JObject> ResetPasswordAsync(string email, string password, string password_confirmation, string token);
+        public Task<JObject> ResetPasswordAsync(string email, string password, string passwordConfirmation, string token);
 
         /// <summary>
         /// Deserializes the JSON string to the <see cref="User"/> model.
