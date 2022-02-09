2dc201a5d54c6f3da41e808931f33b3bcd6b9792
RedMarshal <redmarshal37@gmail.com>
2022-02-09 15:24:06 +0300

    Add teamsPage

---
+++ ViewModels/TeamsPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Equality.Core.ViewModel;
+
+namespace Equality.ViewModels
+{
+    public class TeamsPageViewModel : ViewModel
+    {
+    }
+}
