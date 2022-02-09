fbe9118b6c303c4d06e5d590fc88dc5cb7da0bc9
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-09 19:14:20 +0300

    Add logout and get current user methods to `IUserService`

--- Services/Interfaces/IUserService.cs
+++ Services/Interfaces/IUserService.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
+using Equality.Core.ApiClient;
 using Equality.Models;
 
 using Newtonsoft.Json.Linq;
@@ -9,6 +11,14 @@ namespace Equality.Services
     public interface IUserService
     {
         /// <summary>
+        /// Sends the request to get an authenticated user to the API.
+        /// </summary>
+        /// <param name="token">The api token.</param>
+        /// <returns>Returns the authenticated <see cref="User"/>.</returns>
+        /// <exception cref="ArgumentException" />
+        public Task<User> GetUserAsync(string token);
+
+        /// <summary>
         /// Sends the login request to the API.
         /// </summary>
         /// <param name="email">The user email.</param>
@@ -17,11 +27,20 @@ namespace Equality.Services
         public Task<(User user, string token)> LoginAsync(string email, string password);
 
         /// <summary>
+        /// Sends the logout authenticated user request to the API.
+        /// </summary>
+        /// <param name="token">The api token.</param>
+        /// <returns>Returns the API response.</returns>
+        /// <exception cref="ArgumentException" />
+        public Task<ApiResponseMessage> LogoutAsync(string token);
+
+        /// <summary>
         /// Sends the forgot password request to the API.
         /// </summary>
         /// <param name="email">The user email.</param>
         /// <returns>Returns the server response as a <see cref="JObject"/>.</returns>
-        public Task<JObject> SendResetPasswordTokenAsync(string email);
+        /// <exception cref="ArgumentException" />
+        public Task<ApiResponseMessage> SendResetPasswordTokenAsync(string email);
 
         /// <summary>
         /// Sends the reset password request to the API.
@@ -30,14 +49,16 @@ namespace Equality.Services
         /// <param name="password">The user password.</param>
         /// <param name="passwordConfirmation">The user re-entered password.</param>
         /// <param name="token">The token from an email.</param>
-        /// <returns>Returns the server response as a <see cref="JObject"/>.</returns>
-        public Task<JObject> ResetPasswordAsync(string email, string password, string passwordConfirmation, string token);
+        /// <returns>Returns the API response.</returns>
+        /// <exception cref="ArgumentException" />
+        public Task<ApiResponseMessage> ResetPasswordAsync(string email, string password, string passwordConfirmation, string token);
 
         /// <summary>
         /// Deserializes the JSON string to the <see cref="User"/> model.
         /// </summary>
         /// <param name="data">The JSON string.</param>
         /// <returns>The deserialized <see cref="User"/> model.</returns>
+        /// <exception cref="ArgumentException" />
         public User Deserialize(string data);
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+using Catel;
+
 using Equality.Core.ApiClient;
 using Equality.Models;
 
@@ -20,8 +22,20 @@ namespace Equality.Services
             ApiClient = apiClient;
         }
 
+        public async Task<User> GetUserAsync(string token)
+        {
+            Argument.IsNotNullOrWhitespace(nameof(token), token);
+
+            var response = await ApiClient.WithTokenOnce(token).GetAsync("user");
+
+            return Deserialize(response.Content["data"].ToString());
+        }
+
         public async Task<(User user, string token)> LoginAsync(string email, string password)
         {
+            Argument.IsNotNullOrWhitespace(nameof(email), email);
+            Argument.IsNotNullOrEmpty(nameof(password), password);
+
             Dictionary<string, object> data = new()
             {
                 { "email", email },
@@ -37,20 +51,32 @@ namespace Equality.Services
             return (user, token);
         }
 
-        public async Task<JObject> SendResetPasswordTokenAsync(string email)
+        public async Task<ApiResponseMessage> LogoutAsync(string token)
         {
+            Argument.IsNotNullOrWhitespace(nameof(token), token);
+
+            return await ApiClient.WithTokenOnce(token).PostAsync("logout");
+        }
+
+        public async Task<ApiResponseMessage> SendResetPasswordTokenAsync(string email)
+        {
+            Argument.IsNotNullOrWhitespace(nameof(email), email);
+
             Dictionary<string, object> data = new()
             {
                 { "email", email },
             };
 
-            var response = await ApiClient.PostAsync("forgot-password", data);
-
-            return response.Content;
+            return await ApiClient.PostAsync("forgot-password", data);
         }
 
-        public async Task<JObject> ResetPasswordAsync(string email, string password, string passwordConfirmation, string token)
+        public async Task<ApiResponseMessage> ResetPasswordAsync(string email, string password, string passwordConfirmation, string token)
         {
+            Argument.IsNotNullOrWhitespace(nameof(email), email);
+            Argument.IsNotNullOrEmpty(nameof(password), password);
+            Argument.IsNotNullOrEmpty(nameof(passwordConfirmation), passwordConfirmation);
+            Argument.IsNotNullOrWhitespace(nameof(token), token);
+
             Dictionary<string, object> data = new()
             {
                 { "email", email },
@@ -59,13 +85,13 @@ namespace Equality.Services
                 { "token", token },
             };
 
-            var response = await ApiClient.PostAsync("reset-password", data);
-
-            return response.Content;
+            return await ApiClient.PostAsync("reset-password", data);
         }
 
         public User Deserialize(string data)
         {
+            Argument.IsNotNullOrWhitespace(nameof(data), data);
+
             var user = JsonConvert.DeserializeObject<User>(data, new JsonSerializerSettings
             {
                 ContractResolver = new DefaultContractResolver
