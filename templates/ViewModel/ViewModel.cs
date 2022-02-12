4a933bf33b389387cad934cbfaa8780245fc3aea
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-12 17:47:52 +0300

    Add item templates

---
+++ templates/ViewModel/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+
+using Equality.Core.ViewModel;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : ViewModel
+    {
+        public $safeitemname$()
+        {
+        }
+
+        public override string Title => "View model title";
+
+        #region Properties
+        
+        
+        
+        #endregion
+        
+        #region Commands
+        
+        
+
+        #endregion
+        
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            // TODO: subscribe to events here
+        }
+
+        protected override async Task CloseAsync()
+        {
+            // TODO: unsubscribe from events here
+
+            await base.CloseAsync();
+        }
+    }
+}
---
+++ templates/ViewModelWithValidation/ViewModelWithValidation.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Catel.Data;
+
+using Equality.Core.Validation;
+using Equality.Core.ViewModel;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : ViewModel
+    {
+        public $safeitemname$()
+        {
+            
+
+            ApiFieldsMap = new ()
+            {
+                { nameof(Example), "example" },
+            };
+        }
+
+        public override string Title => "View model title";
+
+        #region Properties
+        
+        public string Example { get; set; }
+
+        #endregion
+        
+        #region Commands
+        
+        
+        
+        #endregion
+        
+        #region Validation
+        
+        protected override void ValidateFields(List<IFieldValidationResult> validationResults)
+        {
+            var validator = new Validator(validationResults);
+        
+            validator.ValidateField(nameof(Example), Example, new()
+            {
+                // TODO: add validation rules
+            });
+        }
+        
+        #endregion
+        
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            // TODO: subscribe to events here
+        }
+
+        protected override async Task CloseAsync()
+        {
+            // TODO: unsubscribe from events here
+
+            await base.CloseAsync();
+        }
+    }
+}
