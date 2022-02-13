f2ccb1dce17c950985d72263df3607b7adb891fb
igorgaming <69463610+igorgaming@users.noreply.github.com>
2022-02-13 11:14:15 +0300

    Add item templates (#12)
    * Custom page class
    
    * Add item templates
    
    * Add readme and update template
    
    * Update readme
    
    * Fix template
    
    * Remove useless `using` directives
    
    * Revert LoginPage changes

---
+++ Core/MVVM/Page.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.ComponentModel;
+
+using UIEventArgs = System.EventArgs;
+
+using Catel;
+using Catel.MVVM;
+using Catel.MVVM.Providers;
+using Catel.MVVM.Views;
+using Catel.Windows;
+using System.Windows;
+
+namespace Equality.Core.MVVM
+{
+    public class Page : System.Windows.Controls.Page, IPage, INavigationView, IView, IViewModelContainer, INotifyPropertyChanged
+    {
+        #region Fields
+        private readonly PageLogic _logic;
+
+        private event EventHandler<EventArgs> _viewLoaded;
+        private event EventHandler<EventArgs> _viewUnloaded;
+        private event EventHandler<Catel.MVVM.Views.DataContextChangedEventArgs> _viewDataContextChanged;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Page"/> class.
+        /// </summary>
+        /// <remarks>It is not possible to inject view models.</remarks>
+        public Page()
+        {
+            if (CatelEnvironment.IsInDesignMode) {
+                return;
+            }
+
+            _logic = new PageLogic(this);
+            _logic.TargetViewPropertyChanged += (sender, e) =>
+            {
+#if !NET && !NETCORE
+                // WPF already calls this method automatically
+                OnPropertyChanged(e);
+
+                PropertyChanged?.Invoke(this, e);
+#else
+                // Do not call this for ActualWidth and ActualHeight WPF, will cause problems with NET 40 
+                // on systems where NET45 is *not* installed
+                if (!string.Equals(e.PropertyName, nameof(ActualWidth), StringComparison.InvariantCulture) &&
+                    !string.Equals(e.PropertyName, nameof(ActualHeight), StringComparison.InvariantCulture))
+                {
+                    PropertyChanged?.Invoke(this, e);
+                }
+#endif
+            };
+
+            _logic.ViewModelChanged += (sender, e) =>
+            {
+                RaiseViewModelChanged();
+            };
+
+            _logic.ViewModelPropertyChanged += (sender, e) =>
+            {
+                OnViewModelPropertyChanged(e);
+
+                ViewModelPropertyChanged?.Invoke(this, e);
+            };
+
+            Loaded += (sender, e) =>
+            {
+                _viewLoaded?.Invoke(this, EventArgs.Empty);
+
+                OnLoaded(e);
+            };
+
+            Unloaded += (sender, e) =>
+            {
+                _viewUnloaded?.Invoke(this, EventArgs.Empty);
+
+                OnUnloaded(e);
+            };
+
+            this.AddDataContextChangedHandler((sender, e) => _viewDataContextChanged?.Invoke(this, new Catel.MVVM.Views.DataContextChangedEventArgs(e.OldValue, e.NewValue)));
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the type of the view model that this user control uses.
+        /// </summary>
+        public Type ViewModelType
+        {
+            get { return _logic.GetValue<PageLogic, Type>(x => x.ViewModelType); }
+        }
+
+        /// <summary>
+        /// Gets the view model that is contained by the container.
+        /// </summary>
+        /// <value>The view model.</value>
+        public IViewModel ViewModel
+        {
+            get { return _logic.GetValue<PageLogic, IViewModel>(x => x.ViewModel); }
+        }
+
+        /// <summary>
+        /// Gets or sets a the view model lifetime management.
+        /// <para />
+        /// By default, this value is <see cref="ViewModelLifetimeManagement"/>.
+        /// </summary>
+        /// <value>
+        /// The view model lifetime management.
+        /// </value>
+        public ViewModelLifetimeManagement ViewModelLifetimeManagement
+        {
+            get { return _logic.GetValue<PageLogic, ViewModelLifetimeManagement>(x => x.ViewModelLifetimeManagement); }
+            set { _logic.SetValue<PageLogic>(x => x.ViewModelLifetimeManagement = value); }
+        }
+        #endregion
+
+        #region Events
+        /// <summary>
+        /// Occurs when a property on the container has changed.
+        /// </summary>
+        /// <remarks>
+        /// This event makes it possible to externally subscribe to property changes of a <see cref="DependencyObject"/>
+        /// (mostly the container of a view model) because the .NET Framework does not allows us to.
+        /// </remarks>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Occurs when the <see cref="ViewModel"/> property has changed.
+        /// </summary>
+        public event EventHandler<EventArgs> ViewModelChanged;
+
+        /// <summary>
+        /// Occurs when a property on the <see cref="ViewModel"/> has changed.
+        /// </summary>
+        public event EventHandler<PropertyChangedEventArgs> ViewModelPropertyChanged;
+
+        /// <summary>
+        /// Occurs when the view is loaded.
+        /// </summary>
+        event EventHandler<EventArgs> IView.Loaded
+        {
+            add { _viewLoaded += value; }
+            remove { _viewLoaded -= value; }
+        }
+
+        /// <summary>
+        /// Occurs when the view is unloaded.
+        /// </summary>
+        event EventHandler<EventArgs> IView.Unloaded
+        {
+            add { _viewUnloaded += value; }
+            remove { _viewUnloaded -= value; }
+        }
+
+        /// <summary>
+        /// Occurs when the data context has changed.
+        /// </summary>
+        event EventHandler<Catel.MVVM.Views.DataContextChangedEventArgs> IView.DataContextChanged
+        {
+            add { _viewDataContextChanged += value; }
+            remove { _viewDataContextChanged -= value; }
+        }
+        #endregion
+
+        #region Methods
+        private void RaiseViewModelChanged()
+        {
+            OnViewModelChanged();
+
+            ViewModelChanged?.Invoke(this, EventArgs.Empty);
+            RaisePropertyChanged(nameof(ViewModel));
+
+            if (_logic.HasVmProperty) {
+                RaisePropertyChanged("VM");
+            }
+        }
+
+        /// <summary>
+        /// Raises the <c>PropertyChanged</c> event.
+        /// </summary>
+        /// <param name="propertyName">The property name to raise the event for.</param>
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Called when the page is loaded.
+        /// </summary>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected virtual void OnLoaded(UIEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Called when the page is unloaded.
+        /// </summary>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        protected virtual void OnUnloaded(UIEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Called when a dependency property on this control has changed.
+        /// </summary>
+        /// <param name="e">The <see cref="PropertyChangedEventArgs"/> instance containing the event data.</param>
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Called when a property on the current <see cref="ViewModel"/> has changed.
+        /// </summary>
+        /// <param name="e">The <see cref="System.ComponentModel.PropertyChangedEventArgs"/> instance containing the event data.</param>
+        protected virtual void OnViewModelPropertyChanged(PropertyChangedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Called when the <see cref="ViewModel"/> has changed.
+        /// </summary>
+        /// <remarks>
+        /// This method does not implement any logic and saves a developer from subscribing/unsubscribing
+        /// to the <see cref="ViewModelChanged"/> event inside the same user control.
+        /// </remarks>
+        protected virtual void OnViewModelChanged()
+        {
+        }
+        #endregion
+    }
+}
---
+++ templates/readme.md
@@ -0,0 +1,12 @@
+# How to use this templates?
+
+1. In **Windows Explorer**, select and open the folder you want to use as template.
+2. Select all files in folder, right-click the selection, and choose **Send to > Compressed (zipped) folder**. The files that you selected are compressed into a _.zip_ file.
+3. Copy the _.zip_ files and paste it in the user item template location.
+The default directory is **%USERPROFILE%\Documents\Visual Studio XXXX\Templates\ItemTemplates\Visual C#**.
+4. Now you can use this templates when creating new item.
+
+For example, if you need to use **Page** template, you should create _.zip_ with this files:
+- Page.vstemplate
+- Page.xaml
+- Page.xaml.cs
