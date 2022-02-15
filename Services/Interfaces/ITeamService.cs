992c7ad8c1ff6bf2d148c344fcf5ff8966992bcc
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-15 11:50:20 +0300

    Add `TeamService`

---
+++ Services/Interfaces/ITeamService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Equality.Core.ApiClient;
+using Equality.Models;
+
+namespace Equality.Services
+{
+    public interface ITeamService
+    {
+        /// <summary>
+        /// Sends the create team request to the API.
+        /// </summary>
+        /// <param name="team">The team.</param>
+        /// <returns>Returns the API response.</returns>
+        /// 
+        /// <exception cref="ArgumentException" />
+        public Task<ApiResponseMessage> CreateAsync(Team team);
+
+        /// <summary>
+        /// Deserializes the JSON string to the <see cref="Team"/> model.
+        /// </summary>
+        /// <param name="data">The JSON string.</param>
+        /// <returns>The deserialized <see cref="Team"/> model.</returns>
+        /// 
+        /// <exception cref="ArgumentException" />
+        public Team Deserialize(string data);
+    }
+}
---
+++ Services/TeamService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Catel;
+
+using Equality.Core.ApiClient;
+using Equality.Core.StateManager;
+using Equality.Models;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Equality.Services
+{
+    public class TeamService : ITeamService
+    {
+        protected IApiClient ApiClient;
+
+        protected IStateManager StateManager;
+
+        public TeamService(IApiClient apiClient, IStateManager stateManager)
+        {
+            ApiClient = apiClient;
+            StateManager = stateManager;
+        }
+
+        public async Task<ApiResponseMessage> CreateAsync(Team team)
+        {
+            Argument.IsNotNullOrWhitespace("IStateManager.ApiToken", StateManager.ApiToken);
+            Argument.IsNotNull(nameof(team), team);
+            Argument.IsNotNullOrWhitespace("team.name", team.Name);
+
+            Dictionary<string, object> data = new()
+            {
+                { "name", team.Name },
+                { "description", team.Description },
+                { "url", team.Url }
+            };
+
+            var response = await ApiClient.WithTokenOnce(StateManager.ApiToken).PostAsync("teams", data);
+
+            return response;
+        }
+
+        public Team Deserialize(string data)
+        {
+            Argument.IsNotNullOrWhitespace(nameof(data), data);
+
+            var team = JsonConvert.DeserializeObject<Team>(data, new JsonSerializerSettings
+            {
+                ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new SnakeCaseNamingStrategy()
+                },
+            });
+
+            return team;
+        }
+    }
+}
