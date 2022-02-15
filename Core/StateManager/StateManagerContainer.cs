78e15b859497e170d983b08a89252e9835d725c6
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-15 15:38:38 +0300

    Move StateManager instance to `StateManagerContainer`

---
+++ Core/StateManager/StateManagerContainer.cs
@@ -0,0 +1,14 @@
+﻿using Catel.IoC;
+
+namespace Equality.Core.StateManager
+{
+    public static class StateManagerContainer
+    {
+        static StateManagerContainer()
+        {
+            Instance = ServiceLocator.Default.ResolveType<IStateManager>();
+        }
+
+        public static IStateManager Instance { get; private set; }
+    }
+}
