f4eae4c8945ba034373dc036070b8d9902ad5dd0
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-18 13:10:49 +0300

    Add `TeamMember` Model

---
+++ Models/TeamMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Equality.Models
+{
+    public class TeamMember : User
+    {
+        public DateTime JoinedAt { get; set; }
+
+        public bool IsCreator { get; set; }
+    }
+}
