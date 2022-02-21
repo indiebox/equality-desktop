93348a3d605763996268ace28b213c7a6f8c36ae
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-02-21 17:53:15 +0300

    Display team logo

---
+++ Controls/TeamLogo/TeamLogo.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Equality.Controls
+{
+    public class TeamLogo : Control
+    {
+        static TeamLogo()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TeamLogo), new FrameworkPropertyMetadata(typeof(TeamLogo)));
+        }
+
+        public string ImagePath
+        {
+            get => (string)GetValue(ImagePathProperty);
+            set => SetValue(ImagePathProperty, value);
+        }
+        public static readonly DependencyProperty ImagePathProperty =
+          DependencyProperty.Register(nameof(ImagePath), typeof(string), typeof(TeamLogo), new PropertyMetadata(string.Empty));
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+        public static readonly DependencyProperty CornerRadiusProperty =
+          DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(TeamLogo), new PropertyMetadata(null));
+    }
+}
