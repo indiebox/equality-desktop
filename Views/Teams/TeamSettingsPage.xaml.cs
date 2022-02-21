127aa13e26ce008a5d6b51a3b41831ba381840a6
RedMarshall37 <81381851+RedMarshall37@users.noreply.github.com>
2022-02-21 19:53:41 +0300

    Add UI for team profile settings (#20)
    * Demo flipper version
    
    * Improve logo flipping
    
    * Fix top marign
    
    * Pre-Design
    
    * Add profile page edit
    
    * Fix margins
    
    * Change design
    
    * Allow open settings page, remove useless code
    
    * Fight with Reviewer I ⚔
    
    * Change `DynamicResource` to `StaticResource` in needed places
    
    Co-authored-by: DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>

---
+++ Views/Teams/TeamSettingsPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Equality.Views
+{
+    public partial class TeamSettingsPage
+    {
+        public TeamSettingsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
