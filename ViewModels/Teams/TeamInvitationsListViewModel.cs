bb4941a649a27d0435c14bb9039ab6d41876a231
RedMarshal <redmarshal37@gmail.com>
2022-02-27 14:34:35 +0300

    Prepare to merdge

---
+++ ViewModels/Teams/TeamInvitationsListViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Catel.Services;
+using Catel.MVVM;
+
+using Equality.Core.ViewModel;
+using Equality.Models;
+using Equality.Core.Extensions;
+using Equality.Services;
+
+namespace Equality.ViewModels
+{
+    public class TeamInvitationsListViewModel : ViewModel
+    {
+
+        public TeamInvitationsListViewModel()
+        {
+        }
+
+        #region Properties
+
+
+        #endregion
+
+        #region Commands
+
+
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+        }
+
+        protected override async Task CloseAsync()
+        {
+            await base.CloseAsync();
+        }
+    }
+}
