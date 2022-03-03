58d68f12d8f82686847b839eb76db61428bc1f82
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-03-03 17:16:06 +0300

    Add InfoCard control

---
+++ Controls/InfoCard/InfoCard.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Equality.Controls
+{
+    public enum CardType
+    {
+        Error,
+        Success,
+    }
+
+    public sealed class InfoCard : Control
+    {
+        static InfoCard()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(InfoCard), new FrameworkPropertyMetadata(typeof(InfoCard)));
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+        public static readonly DependencyProperty TextProperty =
+          DependencyProperty.Register(nameof(Text), typeof(string), typeof(InfoCard), new PropertyMetadata(string.Empty));
+
+        public CardType Type
+        {
+            get => (CardType)GetValue(TypeProperty);
+            set => SetValue(TypeProperty, value);
+        }
+        public static readonly DependencyProperty TypeProperty =
+          DependencyProperty.Register(nameof(Type), typeof(CardType), typeof(InfoCard), new PropertyMetadata(CardType.Error));
+    }
+}
