09a9ed00833570489799611466519e37d0fbad89
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-03-10 17:57:46 +0300

    Expose project image to `Control`

---
+++ Equality/Controls/ProjectImage/ProjectImage.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Equality.Controls
+{
+    public class ProjectImage : TeamLogo
+    {
+        static ProjectImage()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ProjectImage), new FrameworkPropertyMetadata(typeof(ProjectImage)));
+        }
+    }
+}
