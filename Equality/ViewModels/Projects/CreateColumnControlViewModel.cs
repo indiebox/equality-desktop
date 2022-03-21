d638b20280be28ea7441a83cde5972403b9ca190
RedMarshal <redmarshal37@gmail.com>
2022-03-21 12:14:47 +0300

    add control for future

---
+++ Equality/ViewModels/Projects/CreateColumnControlViewModel.cs
@@ -0,0 +1,57 @@
+﻿
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Catel.Data;
+using Catel.MVVM;
+
+using Equality.Http;
+using Equality.Extensions;
+using Equality.Validation;
+using Equality.MVVM;
+using Equality.Models;
+using Equality.Services;
+
+namespace Equality.ViewModels
+{
+    public class CreateColumnControlViewModel : ViewModel
+    {
+        public CreateColumnControlViewModel()
+        {
+        }
+
+        #region Properties
+
+        [Model]
+        public Team Team { get; set; } = new();
+
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Validation
+
+        #endregion
+
+        protected override async Task InitializeAsync()
+        {
+            await base.InitializeAsync();
+
+            // TODO: subscribe to events here
+        }
+
+        protected override async Task CloseAsync()
+        {
+            // TODO: unsubscribe from events here
+
+            await base.CloseAsync();
+        }
+    }
+}
