ec155e3504d2478f66a95274524e21a8df216385
RedMarshal <redmarshal37@gmail.com>
2022-03-23 12:51:58 +0300

    Add bad navigation

--- Equality/ViewModels/Projects/BoardsPageViewModel.cs
+++ Equality/ViewModels/Projects/BoardsPageViewModel.cs
@@ -36,10 +36,14 @@ namespace Equality.ViewModels
 
         IBoardService BoardService;
 
-        public BoardsPageViewModel(IBoardService boardService)
+        INavigationService NavigationService;
+
+        public BoardsPageViewModel(IBoardService boardService, INavigationService navigationService)
         {
             BoardService = boardService;
+            NavigationService = navigationService;
 
+            OpenBoardPage = new Command<Board>(OnOpenOpenBoardPageExecute);
             OpenCreateBoardWindow = new TaskCommand(OnOpenCreateBoardWindowExecuteAsync, () => CreateBoardVm is null);
         }
 
@@ -52,6 +56,16 @@ namespace Equality.ViewModels
         #endregion
 
         #region Commands
+
+        public Command<Board> OpenBoardPage { get; private set; }
+
+        private void OnOpenOpenBoardPageExecute(Board board)
+        {
+            StateManager.SelectedBoard = board;
+
+            NavigationService.Navigate<BoardPageViewModel>();
+        }
+
 
         public TaskCommand OpenCreateBoardWindow { get; private set; }
 
