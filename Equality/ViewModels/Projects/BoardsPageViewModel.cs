27367bdcb62fe4710d0da421409ad6aebbe0d53b
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-03-24 23:16:33 +0300

    Add update board name functional

--- Equality/ViewModels/Projects/BoardsPageViewModel.cs
+++ Equality/ViewModels/Projects/BoardsPageViewModel.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 using Catel.Collections;
+using Catel.Data;
 using Catel.MVVM;
 using Catel.Services;
 
 using Equality.Data;
 using Equality.Extensions;
 using Equality.Helpers;
+using Equality.Http;
 using Equality.Models;
 using Equality.MVVM;
 using Equality.Services;
+using Equality.Validation;
 
 namespace Equality.ViewModels
 {
@@ -29,7 +33,9 @@ namespace Equality.ViewModels
                     new() { Id = 1, Name = "Board" },
                     new() { Id = 2, Name = "Board1" },
                 });
-                ;
+
+                EditableBoard = new Board();
+                FirstValidationHasErrors();
             });
         }
 
@@ -46,6 +52,14 @@ namespace Equality.ViewModels
 
             OpenBoardPage = new Command<Board>(OnOpenOpenBoardPageExecute);
             OpenCreateBoardWindow = new TaskCommand(OnOpenCreateBoardWindowExecuteAsync, () => CreateBoardVm is null);
+            StartEditBoardName = new Command<Board>(OnStartEditBoardNameExecuteAsync);
+            SaveNewBoardName = new TaskCommand(OnSaveNewBoardNameExecuteAsync, () => GetFieldErrors(nameof(NewBoardName)) == string.Empty);
+            CancelEditBoardName = new Command(OnCancelEditBoardNameExecute);
+
+            ApiFieldsMap = new Dictionary<string, string>()
+            {
+                { nameof(NewBoardName), "name" },
+            };
         }
 
         #region Properties
@@ -53,6 +67,11 @@ namespace Equality.ViewModels
         public ObservableCollection<Board> Boards { get; set; } = new();
 
         public CreateBoardControlViewModel CreateBoardVm { get; set; }
+
+        public Board EditableBoard { get; set; } = null;
+
+        [Validatable]
+        public string NewBoardName { get; set; }
 
         #endregion
 
@@ -67,13 +86,54 @@ namespace Equality.ViewModels
             NavigationService.Navigate<BoardPageViewModel, ProjectPageViewModel>();
         }
 
-
         public TaskCommand OpenCreateBoardWindow { get; private set; }
 
         private async Task OnOpenCreateBoardWindowExecuteAsync()
         {
             CreateBoardVm = MvvmHelper.CreateViewModel<CreateBoardControlViewModel>();
             CreateBoardVm.ClosedAsync += CreateBoardVmClosedAsync;
+        }
+
+        public Command<Board> StartEditBoardName { get; private set; }
+
+        private void OnStartEditBoardNameExecuteAsync(Board board)
+        {
+            NewBoardName = board.Name;
+            EditableBoard = board;
+        }
+
+        public Command CancelEditBoardName { get; private set; }
+
+        private void OnCancelEditBoardNameExecute()
+        {
+            EditableBoard = null;
+            NewBoardName = null;
+            Validate(true);
+        }
+
+        public TaskCommand SaveNewBoardName { get; private set; }
+
+        private async Task OnSaveNewBoardNameExecuteAsync()
+        {
+            if (FirstValidationHasErrors()) {
+                return;
+            }
+
+            try {
+                Board board = new()
+                {
+                    Id = EditableBoard.Id,
+                    Name = NewBoardName,
+                };
+
+                var response = await BoardService.UpdateBoardAsync(board);
+
+                EditableBoard.SyncWith(response.Object);
+
+                CancelEditBoardName.Execute();
+            } catch (UnprocessableEntityHttpException e) {
+                HandleApiErrors(e.Errors);
+            }
         }
 
         #endregion
@@ -101,6 +161,23 @@ namespace Equality.ViewModels
 
             } catch (HttpRequestException e) {
                 Debug.WriteLine(e.ToString());
+            }
+        }
+
+        #endregion
+
+        #region Validation
+
+        protected override void ValidateFields(List<IFieldValidationResult> validationResults)
+        {
+            var validator = new Validator(validationResults);
+
+            if (EditableBoard != null) {
+                validator.ValidateField(nameof(NewBoardName), NewBoardName, new()
+                {
+                    new NotEmptyStringRule(),
+                    new MaxStringLengthRule(255),
+                });
             }
         }
 
