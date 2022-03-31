97c03564778905449462e1670b8cb78796b4d67c
RedMarshal <redmarshal37@gmail.com>
2022-03-31 22:07:01 +0300

    drag and drop

--- Equality/Views/Boards/BoardPage.xaml.cs
+++ Equality/Views/Boards/BoardPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 using Equality.Controls;
 using Equality.Models;
@@ -26,50 +27,56 @@ namespace Equality.Views
 
                     Debug.WriteLine("DO");
                     DragColumn = column;
-                    Columns.Remove(column.Column);
+                    Brush brush = new SolidColorBrush(Colors.Black);
+                    column.BorderBrush = brush;
+                    column.Column.Name = "AAAAAAAAAAAAAAAAAAAAAA";
+                    //column.BorderThickness = new Thickness(10);
+                    //Columns.Remove(column.Column);
                 } else if (DragColumn != null && DragColumn != sender) {
 
-                    Debug.WriteLine("Drag");
-                    int dragIndex = Columns.IndexOf(DragColumn.Column);
-                    int senderIndex = Columns.IndexOf(column.Column);
-                    if (dragIndex < senderIndex) {
-                        Columns.Insert(senderIndex + 1, DragColumn.Column);
-                        try {
-                            Columns.RemoveAt(dragIndex);
-                        } catch { }
-                    } else {
-                        int dragIdx = dragIndex + 1;
-                        if (Columns.Count + 1 > dragIdx) {
-                            Columns.Insert(senderIndex + 1, DragColumn.Column);
-                            try {
-                                Columns.RemoveAt(dragIdx);
-                            } catch { }
-                        }
-                    }
+                    //Debug.WriteLine("Drag");
+                    //int dragIndex = Columns.IndexOf(DragColumn.Column);
+                    //int senderIndex = Columns.IndexOf(column.Column);
+                    //if (dragIndex < senderIndex) {
+                    //    Columns.Insert(senderIndex + 1, DragColumn.Column);
+                    //    try {
+                    //        Columns.RemoveAt(dragIndex);
+                    //    } catch { }
+                    //} else {
+                    //    int dragIdx = dragIndex + 1;
+                    //    if (Columns.Count + 1 > dragIdx) {
+                    //        Columns.Insert(senderIndex + 1, DragColumn.Column);
+                    //        try {
+                    //            Columns.RemoveAt(dragIdx);
+                    //        } catch { }
+                    //    }
+                    //}
                 }
             }
         }
 
-        private void ListBox_Drop(object sender, DragEventArgs e)
+        private void ListBox_Drop(object sender, MouseEventArgs e)
         {
-            Column droppedData = e.Data.GetData(typeof(Column)) as Column;
-            Column target = ((ColumnControl)(sender)).DataContext as Column;
+            DragColumn = null;
+            //DragColumn.SetCurrentValue(OpacityProperty, 1.0);
+            //Column droppedData = e.Data.GetData(typeof(Column)) as Column;
+            //Column target = ((ColumnControl)(sender)).DataContext as Column;
 
-            int removedIdx = ListBoxColumns.Items.IndexOf(droppedData);
-            int targetIdx = ListBoxColumns.Items.IndexOf(target);
+            //int removedIdx = ListBoxColumns.Items.IndexOf(droppedData);
+            //int targetIdx = ListBoxColumns.Items.IndexOf(target);
 
-            if (removedIdx < targetIdx) {
-                ListBoxColumns.Items.Insert(targetIdx + 1, droppedData);
-                ListBoxColumns.Items.Remove(removedIdx);
-                //((ObservableCollection<Column>)ListBoxColumns.InputBindings).Insert(targetIdx + 1, droppedData);
-                //_empList.RemoveAt(removedIdx);
-            } else {
-                int remIdx = removedIdx + 1;
-                if (ListBoxColumns.Items.Count + 1 > remIdx) {
-                    ListBoxColumns.Items.Insert(targetIdx, droppedData);
-                    ListBoxColumns.Items.RemoveAt(remIdx);
-                }
-            }
+            //if (removedIdx < targetIdx) {
+            //    ListBoxColumns.Items.Insert(targetIdx + 1, droppedData);
+            //    ListBoxColumns.Items.Remove(removedIdx);
+            //    //((ObservableCollection<Column>)ListBoxColumns.InputBindings).Insert(targetIdx + 1, droppedData);
+            //    //_empList.RemoveAt(removedIdx);
+            //} else {
+            //    int remIdx = removedIdx + 1;
+            //    if (ListBoxColumns.Items.Count + 1 > remIdx) {
+            //        ListBoxColumns.Items.Insert(targetIdx, droppedData);
+            //        ListBoxColumns.Items.RemoveAt(remIdx);
+            //    }
+            //}
         }
     }
 }
