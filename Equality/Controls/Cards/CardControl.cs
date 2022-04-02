513a05351a8bda709cf3912d4cf97af5d8159be6
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-04-02 12:39:47 +0300

    Add `CardControl`

---
+++ Equality/Controls/Cards/CardControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using Equality.Models;
+
+namespace Equality.Controls
+{
+    public class CardControl : Control
+    {
+        static CardControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CardControl), new FrameworkPropertyMetadata(typeof(CardControl)));
+        }
+
+        public Card Card
+        {
+            get => (Card)GetValue(CardProperty);
+            set => SetValue(CardProperty, value);
+        }
+        public static readonly DependencyProperty CardProperty =
+          DependencyProperty.Register(nameof(Card), typeof(Card), typeof(CardControl), new PropertyMetadata(null));
+    }
+}
