fa284d1536a45df8d8dd7c7004ee14dfde9d5d0f
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-04-02 11:52:03 +0300

    Add `Card` model

---
+++ Equality.Core/Models/Interfaces/ICard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Equality.Models
+{
+    public interface ICard
+    {
+        public ulong Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Equality/Models/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Catel.Data;
+
+namespace Equality.Models
+{
+    public class Card : ModelBase, ICard
+    {
+        public ulong Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
