d91e8ab842b5c1b550c61442b2df65d81eecfc18
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-04-02 11:56:17 +0300

    Add `Description` field to `ICard`

--- Equality.Core/Models/Interfaces/ICard.cs
+++ Equality.Core/Models/Interfaces/ICard.cs
@@ -8,6 +8,8 @@ namespace Equality.Models
 
         public string Name { get; set; }
 
+        public string Description { get; set; }
+
         public DateTime CreatedAt { get; set; }
 
         public DateTime UpdatedAt { get; set; }
--- Equality/Models/Card.cs
+++ Equality/Models/Card.cs
@@ -10,6 +10,8 @@ namespace Equality.Models
 
         public string Name { get; set; }
 
+        public string Description { get; set; }
+
         public DateTime CreatedAt { get; set; }
 
         public DateTime UpdatedAt { get; set; }
