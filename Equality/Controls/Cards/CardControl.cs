6a156912e703f72186426fd1caa775fde5e94f91
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-04-03 19:36:15 +0300

    Add commands to controls

--- Equality/Controls/Cards/CardControl.cs
+++ Equality/Controls/Cards/CardControl.cs
@@ -13,6 +13,8 @@ namespace Equality.Controls
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CardControl), new FrameworkPropertyMetadata(typeof(CardControl)));
         }
 
+        #region Properties
+
         public Card Card
         {
             get => (Card)GetValue(CardProperty);
@@ -21,7 +23,49 @@ namespace Equality.Controls
         public static readonly DependencyProperty CardProperty =
           DependencyProperty.Register(nameof(Card), typeof(Card), typeof(CardControl), new PropertyMetadata(null));
 
+        public bool IsEditable
+        {
+            get => (bool)GetValue(IsEditableProperty);
+            set => SetValue(IsEditableProperty, value);
+        }
+        public static readonly DependencyProperty IsEditableProperty =
+          DependencyProperty.Register(nameof(IsEditable), typeof(bool), typeof(CardControl), new PropertyMetadata(false));
+
+        public string NewCardName
+        {
+            get => (string)GetValue(NewCardNameProperty);
+            set => SetValue(NewCardNameProperty, value);
+        }
+        public static readonly DependencyProperty NewCardNameProperty =
+          DependencyProperty.Register(nameof(NewCardName), typeof(string), typeof(CardControl), new PropertyMetadata(string.Empty));
+
+        #endregion
+
         #region Commands
+
+        public ICommand EditCardCommand
+        {
+            get => (ICommand)GetValue(EditCardCommandProperty);
+            set => SetValue(EditCardCommandProperty, value);
+        }
+        public static readonly DependencyProperty EditCardCommandProperty =
+          DependencyProperty.Register(nameof(EditCardCommand), typeof(ICommand), typeof(CardControl), new PropertyMetadata(null));
+
+        public ICommand SaveNewCardNameCommand
+        {
+            get => (ICommand)GetValue(SaveNewCardNameCommandProperty);
+            set => SetValue(SaveNewCardNameCommandProperty, value);
+        }
+        public static readonly DependencyProperty SaveNewCardNameCommandProperty =
+          DependencyProperty.Register(nameof(SaveNewCardNameCommand), typeof(ICommand), typeof(CardControl), new PropertyMetadata(null));
+
+        public ICommand CancelEditCardCommand
+        {
+            get => (ICommand)GetValue(CancelEditCardCommandProperty);
+            set => SetValue(CancelEditCardCommandProperty, value);
+        }
+        public static readonly DependencyProperty CancelEditCardCommandProperty =
+          DependencyProperty.Register(nameof(CancelEditCardCommand), typeof(ICommand), typeof(CardControl), new PropertyMetadata(null));
 
         public ICommand DeleteCardCommand
         {
@@ -31,6 +75,7 @@ namespace Equality.Controls
         public static readonly DependencyProperty DeleteCardCommandProperty =
           DependencyProperty.Register(nameof(DeleteCardCommand), typeof(ICommand), typeof(CardControl), new PropertyMetadata(null));
 
-        #endregion Commands
+
+        #endregion
     }
 }
