c2c08d5e84fb5c7339b755292b4063091d61ea9b
igorgaming <69463610+igorgaming@users.noreply.github.com>
2022-04-15 16:30:54 +0300

    Pusher for leader nominations (#72)
    * Add `SubscribeNominateUserAsync` to `IProjectService`
    
    * Subscribe for pusher event

---
+++ Equality.Core/Services/Interfaces/IProjectService.websockets.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Equality.Models;
+
+namespace Equality.Services
+{
+    public partial interface IProjectServiceBase<TProjectModel, TTeamModel, TLeaderNominationModel, TUserModel>
+        where TProjectModel : class, IProject, new()
+        where TTeamModel : class, ITeam, new()
+        where TLeaderNominationModel : class, ILeaderNomination, new()
+        where TUserModel : class, IUser, new()
+    {
+        /// <summary>
+        /// Subscribe to websocket event for nominate user.
+        /// </summary>
+        /// <param name="project">The board.</param>
+        /// <param name="action">
+        /// The action.
+        /// First argument is the array of leader nominations.
+        /// </param>
+        public Task SubscribeNominateUserAsync(IProject project, Action<TLeaderNominationModel[]> action);
+
+        /// <summary>
+        /// Unsubscribe from websocket event for nominate user.
+        /// </summary>
+        /// <param name="project">The project.</param>
+        public void UnsubscribeNominateUser(IProject project);
+    }
+}
