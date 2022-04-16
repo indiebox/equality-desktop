7e827aa2b97bb991aa242b18c7c1c5df8a68c0c1
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-04-16 12:35:57 +0300

    Remove types from `IBoardService`

--- Equality/Services/Interfaces/IBoardService.cs
+++ Equality/Services/Interfaces/IBoardService.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-using Equality.Models;
+﻿using Equality.Models;
 
 namespace Equality.Services
 {
-    public interface IBoardService : IBoardServiceBase<Board, Project>
+    public interface IBoardService : IBoardServiceBase<Board>
     {
     }
 }
