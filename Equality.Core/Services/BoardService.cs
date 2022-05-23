aa9dcadb16d03b5b13d13b3f1fb0e844a5ac2d17
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-05-23 13:50:14 +0300

    Add `GetBoardAsync` method to `IBoardService`

--- Equality.Core/Services/BoardService.cs
+++ Equality.Core/Services/BoardService.cs
@@ -39,6 +39,18 @@ namespace Equality.Services
             return new(boards, response);
         }
 
+        public async Task<ApiResponseMessage<TBoardModel>> GetBoardAsync(ulong boardId, QueryParameters query = null)
+        {
+            Argument.IsMinimal<ulong>(nameof(boardId), boardId, 1);
+            query ??= new QueryParameters();
+
+            var response = await ApiClient.WithTokenOnce(TokenResolver.ResolveApiToken()).GetAsync(query.Parse($"boards/{boardId}"));
+
+            var board = Json.Deserialize<TBoardModel>(response.Content["data"]);
+
+            return new(board, response);
+        }
+
         public Task<ApiResponseMessage<TBoardModel>> CreateBoardAsync(IProject project, IBoard board, QueryParameters query = null)
             => CreateBoardAsync(project.Id, board, query);
 
