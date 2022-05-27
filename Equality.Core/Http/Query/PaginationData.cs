3a2d4dcdc30e7ebc00d60aa4023aaa4358fcb8bf
DESKTOP-NPVP038\gosha <gosha.sorokin.2002@mail.ru>
2022-05-27 13:29:08 +0300

    Create pagination related classes

---
+++ Equality.Core/Http/Query/PaginationData.cs
@@ -0,0 +1,19 @@
+﻿namespace Equality.Http
+{
+    public class PaginationData
+    {
+        public PaginationData()
+        {
+        }
+
+        /// <summary>
+        /// The count of items per page.
+        /// </summary>
+        public int Count { get; set; }
+
+        /// <summary>
+        /// The cursor for cursor pagination.
+        /// </summary>
+        public string Cursor { get; set; }
+    }
+}
